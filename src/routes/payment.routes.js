9244098c70ecc927d9af59453de5810c34bcd6ca
karan120728-ctrl <karan120728@gmail.com>
2026-05-11 14:06:04 +0530

    fix: handled missing stripe key in payment routes to prevent crash

--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const paymentService = require('../services/payment.service');
 const { protect } = require('../middlewares/auth.middleware');
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = process.env.STRIPE_SECRET_KEY ? require('stripe')(process.env.STRIPE_SECRET_KEY) : null;
 const crypto = require('crypto');
 
 // 1. Create Checkout Session (Protected)
